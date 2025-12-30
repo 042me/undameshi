50e662508c67fb41e7abd4e46f5ef57b007f249d
042me <memos042me@gmail.com>
2025-12-30 22:57:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const cards = [
-    { name: "omikuji", imageUrl: "21.png" },
-    { name: "omikuji", imageUrl: "22.png" },
-    { name: "omikuji", imageUrl: "23.png" },
-    { name: "omikuji", imageUrl: "24.png" },
-    { name: "omikuji", imageUrl: "25.png" },
-     { name: "omikuji", imageUrl: "21.png" },
-     { name: "omikuji", imageUrl: "21.png" },
-    { name: "omikuji", imageUrl: "22.png" },
-    { name: "omikuji", imageUrl: "23.png" },
-    { name: "omikuji", imageUrl: "22.png" },
-    { name: "omikuji", imageUrl: "23.png" },
-    { name: "omikuji", imageUrl: "24.png" },
-    { name: "omikuji", imageUrl: "25.png" },
-    { name: "omikuji", imageUrl: "26.png" },
-    { name: "omikuji", imageUrl: "27.png" },
-    { name: "omikuji", imageUrl: "28.png" },
-    { name: "omikuji", imageUrl: "29.png" },
-    { name: "omikuji", imageUrl: "30.png" }
-];
+window.addEventListener("load", () => {
+    const cards = [
+        { name: "omikuji", imageUrl: "images/21.png" },
+        { name: "omikuji", imageUrl: "images/22.png" },
+        { name: "omikuji", imageUrl: "images/23.png" },
+        { name: "omikuji", imageUrl: "images/24.png" },
+        { name: "omikuji", imageUrl: "images/25.png" },
+        { name: "omikuji", imageUrl: "images/26.png" },
+        { name: "omikuji", imageUrl: "images/27.png" },
+        { name: "omikuji", imageUrl: "images/28.png" },
+        { name: "omikuji", imageUrl: "images/29.png" },
+        { name: "omikuji", imageUrl: "images/30.png" }
+    ];
 
-window.onload = function() {
     const randomIndex = Math.floor(Math.random() * cards.length);
-    const selectedCard = cards[randomIndex];
-
-    // カード画像を表示
-    document.getElementById('card-image').src = selectedCard.imageUrl;
-};
+    document.getElementById("card-image").src = cards[randomIndex].imageUrl;
+});
